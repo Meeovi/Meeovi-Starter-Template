cf65eae9c7078784e56bb75982babc2d60c5335b
Sebastian hilton <bastianh86@outlook.com>
2025-10-28 16:32:48 +0000

    updated 404 page to be dynamic

--- app/error.vue
+++ app/error.vue
@@ -1,17 +1,24 @@
 <template>
     <div>
-        <section class="header5 cid-t7W19CM5wS mbr-fullscreen mbr-parallax-background" id="header5-2">
-            <div class="mbr-overlay" style="opacity: 0.2; background-color: rgb(53, 53, 53);"></div>
+        <section data-bs-version="5.1" class="header18 cid-v0WfSD6Mxt mbr-fullscreen"
+            data-bg-video="https://www.youtube.com/watch?v=2Gg6Seob5Mg" id="header18-3">
 
-            <div class="container">
-                <div class="row justify-content-end">
-                    <div class="col-12 col-lg-7">
-                        <h1 class="mbr-section-title mbr-fonts-style mbr-white mb-3 display-1"><strong>Oops Error
-                                Page</strong></h1>
 
-                        <p class="mbr-text mbr-fonts-style mbr-white display-7">
-                            <a href="/" class="text-primary">Please go back to the homepage.</a></p>
+            <div class="mbr-overlay" style="opacity: 0.5; background-color: rgb(0, 0, 0);"></div>
+            <div class="container-fluid">
+                <div class="row">
+                    <div class="content-wrap col-12 col-md-12">
+                        <h1 class="mbr-section-title mbr-fonts-style mbr-white mb-4 display-1">
+                            <strong>{{ page?.name }}</strong>
+                        </h1>
 
+                        <p class="mbr-fonts-style mbr-text mbr-white mb-4 display-7" v-html="page?.content"></p>
+
+                        <div class="mbr-section-btn">
+                            <NuxtLink class="btn btn-white-outline display-7" :to="page?.link">
+                                {{ page?.repeaterTextBox?.[1]?.name }}
+                            </NuxtLink>
+                        </div>
                     </div>
                 </div>
             </div>
@@ -20,7 +27,23 @@
 </template>
 
 <script setup>
-    useHead({
-        title: 'Oops Error Page',
+    const route = useRoute();
+    const {
+        $directus,
+        $readItems
+    } = useNuxtApp()
+
+    const {
+        data: page
+    } = await useAsyncData('page', () => {
+        return $directus.request($readItems('pages', {
+            filter: {
+                id: {
+                    _eq: 16
+                }
+            },
+            fields: ['*'],
+            limit: 1
+        })).then(response => response?.[0]) // Get first item from response
     })
 </script>
